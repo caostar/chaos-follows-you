6b418d220a4d9c604e8d892e88704d754ed15326
Pablo Lemos de Assis <pablo@tetris.co>
2021-10-01 20:26:36 -0300

    I think I got it.

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -207,9 +207,12 @@ export default class Play extends Scene {
     let randX2 = (Math.random() * window.innerWidth);
     let randY2 = (Math.random() * window.innerHeight);
 
-    const goX = (randX2 / 2 - randX) / zoom;
-    const goY = (randY2 / 2 - randY) / zoom;
+    // const goX = (randX2 / 2 - randX) / zoom;
+    // const goY = (randY2 / 2 - randY) / zoom;
+    const goX = (window.innerWidth / 2 - randX) / zoom;
+    const goY = (window.innerHeight / 2 - randY) / zoom;
     this.moveEmitter(goX,goY);
+    this.newChaos();
 
     this.goZoom(randX, randY, zoom, this.moveEmitterRandomly);
     console.log(viewportX, viewportY)
